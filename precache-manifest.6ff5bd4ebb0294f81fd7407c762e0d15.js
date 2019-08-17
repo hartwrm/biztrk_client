a21a2d7885c6504a3a993ceef38a0cdfc472c025
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-08-17 11:49:39 -0400

    Updates

---
+++ precache-manifest.6ff5bd4ebb0294f81fd7407c762e0d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1583918ce2ec41cca868968b4208ca4",
+    "url": "/biztrk_client/index.html"
+  },
+  {
+    "revision": "2660b21245cea1f228b9",
+    "url": "/biztrk_client/static/css/main.a5a0c7b7.chunk.css"
+  },
+  {
+    "revision": "577e9ec87f4baf89dc08",
+    "url": "/biztrk_client/static/js/2.c4016de7.chunk.js"
+  },
+  {
+    "revision": "2660b21245cea1f228b9",
+    "url": "/biztrk_client/static/js/main.4a35207c.chunk.js"
+  },
+  {
+    "revision": "29692e7f539e9d7927e6",
+    "url": "/biztrk_client/static/js/runtime~main.9a8d8475.js"
+  }
+]);
